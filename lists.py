f7288bba8d93521a5fa4411f1ab4ffde12c29a5e
nevillemthw <nevillemathew2014@gmail.com>
2026-02-15 00:41:37 -0600

    Formatting changes

--- lists.py
+++ lists.py
@@ -16,27 +16,27 @@ reverse
 print
 """)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     N = int(input())
-    
+
     new_list = []
-    
+
     # Use the value of N to iterate over the commands
     for x in range(N):
         # Split the input strings into a list of strings (by default)
         # Convert the indices of the list into int from strings
         split_string = input().split()
-        if split_string[0] == 'insert':
+        if split_string[0] == "insert":
             new_list.insert(int(split_string[1]), int(split_string[2]))
-        elif split_string[0] == 'print':
+        elif split_string[0] == "print":
             print(new_list)
-        elif split_string[0] == 'remove':
+        elif split_string[0] == "remove":
             new_list.remove(int(split_string[1]))
-        elif split_string[0] == 'append':
+        elif split_string[0] == "append":
             new_list.append(int(split_string[1]))
-        elif split_string[0] == 'sort':
+        elif split_string[0] == "sort":
             new_list.sort()
-        elif split_string[0] == 'pop':
+        elif split_string[0] == "pop":
             new_list.pop()
-        elif split_string[0] == 'reverse':
+        elif split_string[0] == "reverse":
             new_list.reverse()
