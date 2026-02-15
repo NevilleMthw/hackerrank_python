be65cf3f1957f12acd93dd4360f7afc4b8048be4
nevillemthw <nevillemathew2014@gmail.com>
2026-02-15 00:50:47 -0600

    Added the comments for understanding

--- 30dayscode/dicts.py
+++ 30dayscode/dicts.py
@@ -9,17 +9,23 @@ sam
 edward
 harry""")
 
+# Input to read the number of cases
 cases = int(input())
 if __name__ == "__main__":
     phonebook = {}
-
+    # First for loop to add to the dictionary based on the number of cases
     for _ in range(cases):
+        # Take input of name and number of one line of string
+        # so the input.split() takes the input as a list
         name, value = input().split()
+        # Append from the list input to the dict
         phonebook[name] = value
-
+    
+    # Second while loop to take in the query inputs while true
     while True:
         try:
             query = input()
+            # Check condition
             if query in phonebook:
                 print(f"{query}={phonebook[query]}")
             else:
